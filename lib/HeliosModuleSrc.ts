92c8f4674796a0aad2b49dd470afad7ff71ceffb
Randall <randall@inator.biz>
2023-10-11 21:54:02 -0700

    add runtime string-to-Helios-Module function for non-rollup scenario

--- lib/HeliosModuleSrc.ts
+++ lib/HeliosModuleSrc.ts
@@ -5,3 +5,17 @@ export type HeliosModuleSrc = string & {
     purpose: string;
     moduleName: string;
 };
+
+export function mkHeliosModule(src: string, filename: string) : HeliosModuleSrc {
+    //@ts-expect-error we didn't finish building yet.  Patience, grasshopper.
+    const module : HeliosModuleSrc = new String(src);
+    const [_, purpose, moduleName] = src.match(
+        /(module|minting|spending|endpoint)\s+([a-zA-Z0-9]+)/m
+    ) || []
+
+    module.srcFile = filename;
+    module.purpose = purpose
+    module.moduleName = moduleName;
+
+    return module        
+}
--- lib/testing/index.ts
+++ lib/testing/index.ts
@@ -3,6 +3,6 @@ export { CapoTestHelper } from "./CapoTestHelper.js"
 export { ADA, addTestContext } from "./types.js"
 export const insufficientInputError = /(need .* lovelace, but only have|transaction doesn't have enough inputs)/;
 
-export  { StellarTestContext } from "./StellarTestContext.js"
+export type  { StellarTestContext } from "./StellarTestContext.js"
 
-Error.stackTraceLimit = 20;
+Error.stackTraceLimit = 100;
