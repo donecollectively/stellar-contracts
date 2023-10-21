b549cd9a3f0add854416b0f708713626cbac84eb
Randall <randall@inator.biz>
2023-10-21 16:33:27 -0700

    clearing obsolete material from CustomTreasury
    ... not fully used in tests yet

--- tests/customizing/CustomTreasury.ts
+++ tests/customizing/CustomTreasury.ts
@@ -4,6 +4,7 @@ import { StellarTxnContext } from "../../src/StellarTxnContext.js";
 import { CustomMinter } from "./CustomMinter.js";
 import { DefaultCapo } from "../../src/DefaultCapo.js";
 
+//@ts-expect-error importing a module provided by Rollup
 import contract from "./CustomTreasury.hl";
 import { RoleMap, variantMap } from "../../src/delegation/RolesAndDelegates.js";
 import { BasicMintDelegate } from "../../src/delegation/BasicMintDelegate.js";
@@ -21,32 +22,6 @@ export class CustomTreasury extends DefaultCapo<CustomMinter> {
 
     static get defaultParams() {
         return {}
-    }
-
-    get roles() : RoleMap {
-        const inherited = super.roles;
-        const {mintDelegate, ... othersInherited} = inherited;
-        return {
-            ...othersInherited,
-            noDefault: variantMap<DefaultMinter>({ 
-            }),
-            mintDelegate: variantMap<BasicMintDelegate>({ 
-                ... mintDelegate,
-                failsWhenBad: {
-                    delegateClass:  BasicMintDelegate,
-                    validateConfig(args) {
-                        //@ts-expect-error so we can force test validation
-                        if (args.bad) {
-                            //note, this isn't the normal way of validating.
-                            //  ... usually it's a good field name whose value is missing or wrong.
-                            //  ... still, this conforms to the ErrorMap protocol good enough for testing.
-                            return {bad:  [ "must not be provided" ]}
-                        }
-                    }
-                },
-                
-            })
-        }
     }
 
     @Activity.redeemer
