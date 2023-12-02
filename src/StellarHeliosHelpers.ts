affb7c47d8240c721557133250191a5730d70361
Randall <randall@inator.biz>
2023-12-02 09:14:41 -0800

    fix type import of HeliosModuleSrc

--- src/StellarHeliosHelpers.ts
+++ src/StellarHeliosHelpers.ts
@@ -4,6 +4,6 @@
 //  ... where a second imported .hl file in a single .ts file
 //  ... causes the dts rollup plugin to not find the second .hl file
 import shh from "./StellarHeliosHelpers.hl";
-import { HeliosModuleSrc } from "./HeliosModuleSrc.js";
+import type { HeliosModuleSrc } from "./HeliosModuleSrc.js";
 
 export const StellarHeliosHelpers: HeliosModuleSrc = shh;
