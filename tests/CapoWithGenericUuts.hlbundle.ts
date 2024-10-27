77308195c8856af47dfb21f24641c9be998992fa
Randall <randall@inator.biz>
2024-10-27 12:58:52 -0700

    remove custom capo to save time in every build
     - the custom-capo scenario works fine for now

--- tests/CapoWithGenericUuts.hlbundle.ts
+++ tests/CapoWithGenericUuts.hlbundle.ts
@@ -1,15 +1,15 @@
 import { CapoHeliosBundle } from "../src/CapoHeliosBundle";
 import StructDatumTester from "../src/testing/StructDatumTester.hlbundle.js";
 
-import StructDatumTesterScript from "../src/testing/StructDatumTester.hl";
+// import StructDatumTesterScript from "../src/testing/StructDatumTester.hl";
 /**
  * A CapoHeliosBundle subclass that can be used with generic UUTs.
  */
 export default class CapoBundleWithGenericUuts extends CapoHeliosBundle {
-    get modules() {  // optional
-        return [
-            ...super.modules,
-            StructDatumTesterScript,
-        ];
-    }
+    // get modules() {  // optional
+    //     return [
+    //         ...super.modules,
+    //         StructDatumTesterScript,
+    //     ];
+    // }
 }
