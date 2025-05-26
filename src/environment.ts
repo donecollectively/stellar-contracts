0e5aa145f8f8ebe9fb95815f6c477e45d9beb922
Randall <randall@inator.biz>
2025-05-26 07:08:00 -0700

    fix detection of Vite environemnt

--- src/environment.ts
+++ src/environment.ts
@@ -1,16 +1,27 @@
-let DEBUG;
-let CARDANO_NETWORK;
-let NODE_ENV;
-let OPTIMIZE;
+let DEBUG : number;
+let CARDANO_NETWORK: string;
+let NODE_ENV: string;
+let OPTIMIZE: number;
+
+const isNodeJS =typeof process !== 'undefined' 
+const cwd = isNodeJS ? process.cwd() : ""
 
 let anyNEXTjsCue = process.env.NEXT_PUBLIC_DEBUG || 
     process.env.NEXT_PUBLIC_CARDANO_NETWORK || 
     process.env.NEXT_PUBLIC_OPTIMIZE || 
     process.env.NODE_ENV;
 
-//@ts-expect-error
-if (import.meta?.env && import.meta.env) {
-    console.log("VITE env");
+if (
+    ( () => {
+        try {
+            //@ts-expect-error
+            return import.meta.env
+        } catch (e) {
+            return undefined
+        }
+    }
+)()) {
+    console.log("VITE env - using import.meta.env");
     //@ts-expect-error
     DEBUG = parseInt(import.meta.env.VITE_DEBUG || "0");
     //@ts-expect-error
@@ -20,13 +31,13 @@ if (import.meta?.env && import.meta.env) {
     //@ts-expect-error
     OPTIMIZE = parseInt(import.meta.env.VITE_OPTIMIZE || "0");
 } else if (anyNEXTjsCue) {
-    console.log("nextjs worker env");
+    console.log("nextjs worker env - using NODE_ENV and NEXT_PUBLIC_*");
     DEBUG = parseInt(process.env.NEXT_PUBLIC_DEBUG || "0");
     CARDANO_NETWORK = process.env.NEXT_PUBLIC_CARDANO_NETWORK || "preprod";
     NODE_ENV = process.env.NODE_ENV || "development";
     OPTIMIZE = parseInt(process.env.NEXT_PUBLIC_OPTIMIZE || "0");
 } else {
-    console.log("non-vite, non-nextjs - process.env");
+    console.log("non-vite, non-nextjs - consulting process.env keys directly");
     DEBUG = parseInt(process.env.DEBUG || "0");
     CARDANO_NETWORK = process.env.CARDANO_NETWORK || "preprod";
     NODE_ENV = process.env.NODE_ENV || "development";
@@ -34,11 +45,12 @@ if (import.meta?.env && import.meta.env) {
 }
 
 // console.log(process.env);
-console.log("env:", {DEBUG, CARDANO_NETWORK, NODE_ENV, OPTIMIZE});
+console.log("env:", {DEBUG, CARDANO_NETWORK, NODE_ENV, OPTIMIZE, cwd});
 
 export const environment = {
     DEBUG,
     CARDANO_NETWORK,
     NODE_ENV,
     OPTIMIZE,
+    cwd,
 };
