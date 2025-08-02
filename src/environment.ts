174e03763bdbf2e247bf501af6c0881750ead821
Randall <randall@inator.biz>
2025-08-02 09:43:09 -0700

    add BF_API_KEY to pre-defined attributes in environment utility

--- src/environment.ts
+++ src/environment.ts
@@ -2,6 +2,7 @@ let DEBUG : number;
 let CARDANO_NETWORK: string;
 let NODE_ENV: string;
 let OPTIMIZE: number;
+let BF_API_KEY: string;
 
 const isNodeJS =typeof process !== 'undefined' 
 const cwd = isNodeJS ? process.cwd() : ""
@@ -29,23 +30,27 @@ if (
     //@ts-expect-error
     NODE_ENV = import.meta.env.DEV ? "development" : "production";
     //@ts-expect-error
+    BF_API_KEY = import.meta.env.VITE_BF_API_KEY || "";
+    //@ts-expect-error
     OPTIMIZE = parseInt(import.meta.env.VITE_OPTIMIZE || "0");
 } else if (anyNEXTjsCue) {
     console.log("nextjs worker env - using NODE_ENV and NEXT_PUBLIC_*");
     DEBUG = parseInt(process.env.NEXT_PUBLIC_DEBUG || "0");
     CARDANO_NETWORK = process.env.NEXT_PUBLIC_CARDANO_NETWORK || "preprod";
     NODE_ENV = process.env.NODE_ENV || "development";
+    BF_API_KEY = process.env.NEXT_PUBLIC_BF_API_KEY || "";
     OPTIMIZE = parseInt(process.env.NEXT_PUBLIC_OPTIMIZE || "0");
 } else {
     console.log("non-vite, non-nextjs - consulting process.env keys directly");
     DEBUG = parseInt(process.env.DEBUG || "0");
     CARDANO_NETWORK = process.env.CARDANO_NETWORK || "preprod";
     NODE_ENV = process.env.NODE_ENV || "development";
+    BF_API_KEY = process.env.BF_API_KEY || "";
     OPTIMIZE = parseInt(process.env.OPTIMIZE || "0");
 }
 
 // console.log(process.env);
-console.log("env:", {DEBUG, CARDANO_NETWORK, NODE_ENV, OPTIMIZE, cwd});
+console.log("env:", {DEBUG, CARDANO_NETWORK, NODE_ENV, BF_API_KEY, OPTIMIZE, cwd, });
 
 /**
  * @public
@@ -53,6 +58,7 @@ console.log("env:", {DEBUG, CARDANO_NETWORK, NODE_ENV, OPTIMIZE, cwd});
 export const environment = {
     DEBUG,
     CARDANO_NETWORK,
+    BF_API_KEY,
     NODE_ENV,
     OPTIMIZE,
     cwd,
